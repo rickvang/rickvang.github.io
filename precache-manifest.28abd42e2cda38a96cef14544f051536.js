8199350b68a9ed1fe108def29be4d4d86b861d27
rickvang <rvang@adobe.com>
2019-04-23 09:20:58 -0600

    initial commit
    prototype for archon.

---
+++ precache-manifest.28abd42e2cda38a96cef14544f051536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "588ac8fb0146b58f023a",
+    "url": "/static/css/main.64510a23.chunk.css"
+  },
+  {
+    "revision": "588ac8fb0146b58f023a",
+    "url": "/static/js/main.588ac8fb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7f0379b481ce86948517",
+    "url": "/static/css/2.3870777a.chunk.css"
+  },
+  {
+    "revision": "7f0379b481ce86948517",
+    "url": "/static/js/2.7f0379b4.chunk.js"
+  },
+  {
+    "revision": "8adcc9a1f1e8227311d94edbfc123a3b",
+    "url": "/index.html"
+  }
+];
